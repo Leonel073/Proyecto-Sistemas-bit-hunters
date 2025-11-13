758a4a661ad433118a8f25317b838cca646db0d9
Rudy Mena Colque <menacolmenacolquerudyleonel@gmail.com>
2025-11-13 09:15:50 -0400

    Modifique el modulo de Supervisor de Operador

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -28,4 +28,26 @@ class Empleado extends Authenticatable
     {
         return $this->passwordHash;
     }
+
+    /**
+     * Obtiene el perfil específico de Supervisor de Operadores.
+     */
+    public function supervisorOperador()
+    {
+        // Un Empleado TIENE UN perfil de SupervisorOperador
+        return $this->hasOne(SupervisorOperador::class, 'idEmpleado', 'idEmpleado');
+    }
+
+    /**
+     * (Recomendado) Añade también las relaciones para los otros roles
+     */
+    public function operador()
+    {
+        return $this->hasOne(Operador::class, 'idEmpleado', 'idEmpleado');
+    }
+    
+    public function tecnico()
+    {
+        return $this->hasOne(Tecnico::class, 'idEmpleado', 'idEmpleado');
+    }
 }
